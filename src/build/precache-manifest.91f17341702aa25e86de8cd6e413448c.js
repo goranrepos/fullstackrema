b48bff81debd00727bc1305a63daaadcf9c5e73f
pixelresponse <naumovskig@yahoo.com>
2020-02-08 17:30:32 +0000

    add rema code

---
+++ src/build/precache-manifest.91f17341702aa25e86de8cd6e413448c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6422be69853040b292cfb97a52ce22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c45b4825903bd79ea14a",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "a57ec4aa79f0166cff87",
+    "url": "/static/js/2.2f5453e8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.2f5453e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45b4825903bd79ea14a",
+    "url": "/static/js/main.65f5821c.chunk.js"
+  },
+  {
+    "revision": "e6da9a8bd2dd2bef4ccc",
+    "url": "/static/js/runtime-main.4f62c7f0.js"
+  }
+]);
